11dc4cd6409c2a987c56b4d76009685da54914b7
REÇ <ecoban@sutas.com.tr>
2019-01-04 15:26:38 +0300

    a list that you can add or delete from

--- App.js
+++ App.js
@@ -20,16 +20,23 @@ export default class App extends Component {
   placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        // places: prevState.places.concat(placeName) // for Scrollview in PlaceList.js
+        places: prevState.places.concat({
+          key: Math.random(), 
+          value: placeName
+        })
       };
     });
   };
 
-  placeDeletedHandler = index => {
+  placeDeletedHandler = key => {
     this.setState(prevState => {
       return {
-        places: prevState.places.filter((place, i) => {
-          return i !== index;
+        // places: prevState.places.filter((place, i) => { // for Scrollview in PlaceList.js
+        //   return i !== index;
+      //})
+        places: prevState.places.filter(place => {
+          return place.key !== key;
         })
       };
     });
--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,27 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView } from 'react-native';
+import { StyleSheet, Text, ScrollView, FlatList } from 'react-native';
 
 import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
-    const placesOutput = props.places.map((place, i) => (
-        <ListItem 
-            key={i} 
-            placeName={place} 
-            onItemPressed={() => props.onItemDeleted(i)}/>
-        // <Text key={i}>{place}</Text>
-    ));
-    return (
-        <ScrollView style={styles.listContainer}>
-            {placesOutput}
-        </ScrollView>
-    );
+    // const placesOutput = props.places.map((place, i) => (
+    //     <ListItem 
+    //         key={i} 
+    //         placeName={place} 
+    //         onItemPressed={() => props.onItemDeleted(i)}/>
+    //     // <Text key={i}>{place}</Text>
+    // ));
+    // return (
+    //     <ScrollView style={styles.listContainer}>
+    //         {placesOutput}
+    //     </ScrollView>
+    // );
+
+    return <FlatList
+        style={styles.listContainer}
+        data={props.places}
+        renderItem={(info) => (
+            <ListItem
+                placeName={info.item.value}
+                onItemPressed={() => props.onItemDeleted(info.item.key)}
+            />
+        )} />;
 };
 
 const styles = StyleSheet.create({
     listContainer: {
         width: "100%"
-      }
+    }
 });
 
 export default placeList;
