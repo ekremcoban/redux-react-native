0aed019a0f70f5f3421a1e0cb679faf2a49fa5d6
REÇ <ecoban@sutas.com.tr>
2019-01-07 14:04:34 +0300

    add and delete items of list

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import ListItem from './src/components/ListItem/ListItem';
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
 
 export default class App extends Component {
   state = {
-    places: []
+    places: [],
+    selectedPlace: null
   };
 
   placeAddedHandler = placeName => {
@@ -32,24 +27,45 @@ export default class App extends Component {
     });
   };
 
-  placeDeletedHandler = key => {
+  placeDeletedHandler = () => {
     this.setState(prevState => {
       return {
-        // places: prevState.places.filter((place, i) => { // for Scrollview in PlaceList.js
-        //   return i !== index;
-      //})
         places: prevState.places.filter(place => {
-          return place.key !== key;
-        })
+          return place.key !== prevState.selectedPlace.key;
+        }),
+        selectedPlace: null
       };
     });
   };
 
+  modalClosedHandler = () => {
+    this.setState({
+      selectedPlace: null
+    });
+  };
+
+  placeSelectedHandler = key => {
+    this.setState(prevState => {
+      return {
+        selectedPlace: prevState.places.find(place => {
+          return place.key === key;
+      })
+    };
+  });
+};
+
   render() {
     return (
       <View style={styles.container}>
+      <PlaceDetail 
+        selectedPlace={this.state.selectedPlace} 
+        onItemDeleted={this.placeDeletedHandler}
+        onItemClosed={this.modalClosedHandler}
+        />
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList places={this.state.places} onItemDeleted={this.placeDeletedHandler}/>
+        <PlaceList 
+          places={this.state.places} 
+          onItemSelected={this.placeSelectedHandler}/>
       </View>
     );
   }
--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,32 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
 
 import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
-    // const placesOutput = props.places.map((place, i) => (
-    //     <ListItem 
-    //         key={i} 
-    //         placeName={place} 
-    //         onItemPressed={() => props.onItemDeleted(i)}/>
-    //     // <Text key={i}>{place}</Text>
-    // ));
-    // return (
-    //     <ScrollView style={styles.listContainer}>
-    //         {placesOutput}
-    //     </ScrollView>
-    // );
-
-    return <FlatList
-        style={styles.listContainer}
-        data={props.places}
-        renderItem={(info) => (
-            <ListItem
-                placeName={info.item.name}
-                placeImage={info.item.image}
-                onItemPressed={() => props.onItemDeleted(info.item.key)}
-            />
-        )} />;
+    return (
+        <FlatList
+            style={styles.listContainer}
+            data={props.places}
+            renderItem={(info) => (
+                <ListItem
+                    placeName={info.item.name}
+                    placeImage={info.item.image}
+                    onItemPressed={() => props.onItemSelected(info.item.key)}
+                />
+        )} />
+    );
 };
 
 const styles = StyleSheet.create({
